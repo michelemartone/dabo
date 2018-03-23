7a667ed41b8959200fd32358422fb13b41bb510b
Michele Martone <michelemartone@users.sourceforge.net>
2018-03-23 18:18:16 +0100

    can now render further files to HTML form

--- test.sh
+++ test.sh
@@ -10,12 +10,16 @@ for PKG in librsb ; do
 	test -d $TD
 	cd $TD
 	bash $TS 2>&1 1> $LF && echo SUCCESS || echo FAILURE
-	HS=$TD/`basename $TS`.html
-	HOME=. vim -E $TS -c 'syntax on' -c 'TOhtml' -c "w $HS" -c 'qall!'
-	test -f $HS
+	cp $TS .
+	FL="test.sh `find -name '*.c' -o -iname '*.h'`"
+	ls -l $FL 
+	for TF in $FL ; do
+		HS=$TF.html
+		HOME=. vim -E $TF -c 'syntax on' -c 'TOhtml' -c "w $HS" -c 'qall!'
+		test -f $HS
+		#elinks $HS
+	done
 	ls -l
-	# elinks $HS
-	# cp $HS ~/
 	rm -fR $TD
 	test -f $LF
 	#nl $LF
