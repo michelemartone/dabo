da7d1074cf49818c7cbbdfa96ef3f59b70e2ebc5
Michele Martone <michelemartone@users.sourceforge.net>
2018-03-26 11:17:48 +0200

    minimal cmake example in.

---
+++ cmake/test.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -e
+set -x
+#export LC_ALL="en_US.UTF-8"
+#export LC_ALL=""
+#export LANG="de"
+rm -fR Example
+#shar -s user@example -m  Example | grep -v $USER > Example.shar && vim Example.shar
+./Example.shar
+cd Example # originally from the cmake distribution. see https://cmake.org/examples/
+cmake .
+make
+find -name helloDemo
+test -f Demo/helloDemo
+test -x Demo/helloDemo
+Demo/helloDemo | grep World
+cd -
+rm -fR Example
