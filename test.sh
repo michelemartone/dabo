578cc0383ddab1a159be0ce98d02914c1e4d3282
Michele Martone <michelemartone@users.sourceforge.net>
2018-03-26 18:19:27 +0200

    script verbosity can now be adjusted

--- test.sh
+++ test.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+VL=0
 set -e
-set -x
+test $VL -ge 1 && set -x
 PKGS='librsb intel cmake'
 for PKG in ${1:-$PKGS} ; do
 	test -d $PKG
@@ -9,23 +10,25 @@ for PKG in ${1:-$PKGS} ; do
 	LF=`pwd`/$PKG.log
 	test -f $TS
 	TD=`mktemp -d /dev/shm/$PKG-XXXX`
+	DN=/dev/null
 	test -d $TD
 	cd $TD
-	for f in $SD/*.shar ; do cp $f . ; done || true
+	( for f in $SD/*.shar ; do test -f $f && cp $f . ; done || true ; ) > $DN
 	cp $TS .
-	bash -e $TS 2>&1 1> $LF && echo SUCCESS || echo FAILURE
+	( bash -e $TS 2>&1 ; ) 1> $LF \
+		&& echo "SUCCESS: $PKG" || echo "FAILURE: $PKG"
 	FL="test.sh `find -name '*.c' -o -iname '*.h' -o -iname '*.F90'`"
-	ls -l $FL 
+	test $VL -ge 1 && ls -l $FL 
 	for TF in $FL ; do
 		HS=$TF.html
-		HOME=. vim -E $TF -c 'syntax on' -c 'TOhtml' -c "w $HS" -c 'qall!'
+		HOME=. vim -E $TF -c 'syntax on' -c 'TOhtml' -c "w $HS" -c 'qall!' 2>&1 > $DN
 		test -f $HS
 		#elinks $HS
 	done
-	ls -l
+	test $VL -ge 1 && ls -l
 	rm -fR $TD
 	test -f $LF
-	nl $LF
-	cd -
+	test $VL -ge 2 && nl $LF
+	cd - 2>&1 > $DN
 done
 echo TERMINATED
