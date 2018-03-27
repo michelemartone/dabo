981b9b4d3f5b41d681846901c71b3b31ef1809f8
Michele Martone <michelemartone@users.sourceforge.net>
2018-03-27 23:56:25 +0200

    homonimy (name clash) led to overwriting of source script through results. fix.

--- test.sh
+++ test.sh
@@ -23,10 +23,10 @@ for PKG in ${@:-$PKGS} ; do
 		&& { TR="pass"; echo "PASS: $PKG"; PASS+=" $PKG"; } \
 		|| { TR="fail"; echo "FAIL: $PKG"; FAIL+=" $PKG"; }
 	#mailx -s test-batch-${PKG}:${TR} -a ${LF} -S from="${EMAIL}" "${EMAIL}"
-	FL="test.sh `find -name '*.c' -o -iname '*.h' -o -iname '*.F90'`"
+	FL="test.sh `find -maxdepth 1 -name '*.c' -o -iname '*.h' -o -iname '*.F90'`"
 	test "$VL" -ge 1 && ls -l -- $FL 
 	for TF in $FL ; do
-		HS=${SD}.html # HS=$TF.html
+		HS=${SD}-`basename ${TF}`.html # HS=$TF.html
 		HOME=. vim -E $TF -c 'syntax on' -c 'TOhtml' -c "w! $HS" -c 'qall!' 2>&1 > $DN
 		test -f $HS
 		#elinks $HS
@@ -47,8 +47,8 @@ test -z "$FAIL" && test -n "$PASS" && CMT+="All tests passed."
 test -n "$FAIL" && test -z "$PASS" && CMT+="All tests failed."
 test -n "$FAIL" && test -n "$PASS" && CMT+="Some tests failed."
 pwd
-test -n "$FAIL" && for t in $FAIL ; do for f in $t.{log,html} ; do mv $f failed-$f ; done; done
-test -n "$PASS" && for t in $PASS ; do for f in $t.{log,html} ; do mv $f passed-$f ; done; done
+test -n "$FAIL" && for t in $FAIL ; do for f in $t*.{log,html} ; do mv $f failed-$f ; done; done
+test -n "$PASS" && for t in $PASS ; do for f in $t*.{log,html} ; do mv $f passed-$f ; done; done
 ls -- *.html *.log | sort | sed 's/\(.*$\)/<a href="\1">\1<\/a>\n<br\/>/g' > index.html
 #SL="${FAIL:+FAIL:}${FAIL} ${PASS:+PASS:}${PASS}"
 SL="$CMT"
