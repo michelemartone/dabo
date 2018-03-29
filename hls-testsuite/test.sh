32a21cc382d18751050691e964fb52f2055f1c96
Michele Martone <michelemartone@users.sourceforge.net>
2018-03-29 16:32:36 +0200

    hls-testsuite template test in.

---
+++ hls-testsuite/test.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -e
+set -x
+echo " == Write me please == "
+test -d ~/src/testsuite
+cd      ~/src/testsuite
+# execute the HLS Testsuite, or part of it.
+false
+#bash bin/testinit
+#bash sw-tests-serial.sh
--- test.sh
+++ test.sh
@@ -13,7 +13,7 @@ TO=${SCAMC_TIMEOUT:="$TO"}
 [[ "$TO" =~ ^[0-9]+[ms]$ ]] || { echo "ERROR: SCAMC_TIMEOUT=$SCAMC_TIMEOUT: <number>[ms], e.g. $TO, 1m, ..!"; false; }
 test "$VL" -ge 3 && set -x
 if test "$VL" -ge 1 ; then VMD=-v; VS=''; VCP=-v; else VMD=''; VS=-q; VCP=''; fi
-TSTS='false true filesystems gcc intel git svn cmake autotools librsb octave lrztools matlab spack python-3.0.1 gromacs timeout'
+TSTS='false true filesystems gcc intel git svn cmake autotools librsb octave lrztools matlab spack python-3.0.1 gromacs timeout hls-testsuite'
 PASS=''
 FAIL=''
 POFL=''
