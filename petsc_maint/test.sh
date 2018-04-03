64e37eb1125b913c2089dcc8960a9ec411a2317b
Michele Martone <michelemartone@users.sourceforge.net>
2018-04-03 15:07:00 +0200

    commented obsolete petsc_maint code

--- petsc_maint/test.sh
+++ petsc_maint/test.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 set -e
 test "$LRZ_SYSTEM_SEGMENT" = CMUC2
-for MP in ${MODULEPATH//:/ } ; do 
-	if [[ "$MP" =~ $USER ]] ; then 
-		module unuse $MP; echo "unloading module $MP" 
-	fi
-done
+#for MP in ${MODULEPATH//:/ } ; do 
+#	if [[ "$MP" =~ $USER ]] ; then 
+#		module unuse $MP; echo "unloading module $MP" 
+#	fi
+#done
 #PML=`module avail petsc | grep -v -- --`
 #PML="petsc/3.8"
 PML="petsc/3.8 petsc/3.8_debug petsc/3.8c petsc/3.8c_debug"
