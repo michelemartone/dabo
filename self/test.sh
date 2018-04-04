d19d3d7665698d6a915238cbbd36a198cc9eb244
Michele Martone <michelemartone@users.sourceforge.net>
2018-04-04 11:30:14 +0200

    SCAMC_RESULTS_DIR and SCAMC_EMAIL testing.

--- self/test.sh
+++ self/test.sh
@@ -5,20 +5,20 @@ set -x
 test -n "$SCAMC_SCRIPT"
 test -f "$SCAMC_SCRIPT"
 test -x "$SCAMC_SCRIPT"
-unset SCAMC_EMAIL
-unset SCAMC_VERBOSITY
-unset SCAMC_TIMEOUT
-unset SCAMC_RESULTS_DIR
 #
 function build_test()
 {
 	TD=self_internal
 	TS=$TD/test.sh
-	rm -fR $TD failed.log passed.log
+	rm -fR $TD ${SCAMC_RESULTS_DIR}failed.log ${SCAMC_RESULTS_DIR}passed.log
 	mkdir  $TD
 	echo "$@" > $TS
 	test -f $TS
 	$SCAMC_SCRIPT $TD
+}
+function build_failing_test()
+{
+	if build_test $@ ; then false; else true; fi
 }
 function post_any()
 {
@@ -28,25 +28,49 @@ function post_any()
 }
 function post_pass()
 {
-	test ! -f failed.log
-	test   -f passed.log
-	nl passed.log
-	rm        passed.log
+	test ! -f ${SCAMC_RESULTS_DIR}failed.log
+	test   -f ${SCAMC_RESULTS_DIR}passed.log
+	nl ${SCAMC_RESULTS_DIR}passed.log
+	rm        ${SCAMC_RESULTS_DIR}passed.log
 	post_any
 }
 function post_fail()
 {
-	test   -f failed.log
-	test ! -f passed.log
-	nl failed.log
-	rm        failed.log
+	test   -f ${SCAMC_RESULTS_DIR}failed.log
+	test ! -f ${SCAMC_RESULTS_DIR}passed.log
+	nl ${SCAMC_RESULTS_DIR}failed.log
+	rm        ${SCAMC_RESULTS_DIR}failed.log
 	post_any
 }
+function basic_tests()
+{
+	build_test true
+	post_pass
+	build_test false
+	post_fail
+}
+unset SCAMC_EMAIL
+unset SCAMC_VERBOSITY
+unset SCAMC_TIMEOUT
+unset SCAMC_RESULTS_DIR
+#
 export SCAMC_TIMEOUT=1s
 export SCAMC_VERBOSITY=3
-build_test true
-post_pass
-build_test fail
-post_fail
 build_test 'sleep 2'
 post_fail
+basic_tests
+#
+#
+export SCAMC_RESULTS_DIR=`pwd`/custom_results_dir/
+test -n $SCAMC_RESULTS_DIR
+rm -fR  "$SCAMC_RESULTS_DIR"
+mkdir -p "$SCAMC_RESULTS_DIR"
+test -d "$SCAMC_RESULTS_DIR"
+build_test true
+post_pass
+build_test false
+post_fail
+#
+export SCAMC_EMAIL=wrong_address
+build_failing_test true
+#
