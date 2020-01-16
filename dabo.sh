42197dccc9a2b9eff6963c156218d2182a5c58ca
Michele Martone <michelemartone@users.sourceforge.net>
2020-01-16 18:41:55 +0100

    add license

--- dabo.sh
+++ dabo.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # 
-# Copyright 2018 Michele MARTONE
+# Copyright 2018-2020 Michele MARTONE
 # 
 # Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
 # 
